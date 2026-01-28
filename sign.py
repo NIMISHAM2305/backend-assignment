bb177e7c74e6c6539f0d1f39515e3be7e641d0af
NIMISHAM2305 <nimishamamtani2305@gmail.com>
2026-01-28 15:26:06 +0530

    commit

---
+++ sign.py
@@ -0,0 +1,9 @@
+import hmac
+import hashlib
+
+secret = "testsecret"
+
+body = b'{"message_id":"m1","from":"+919876543210","to":"+14155550100","ts":"2025-01-15T10:00:00Z","text":"Hello"}'
+
+sig = hmac.new(secret.encode(), body, hashlib.sha256).hexdigest()
+print(sig)
